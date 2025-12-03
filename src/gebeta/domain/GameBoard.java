bdae5d8ba358307f3428cc11471ff3892a642c88
Emran Mohammed <emran.seid@aastustudent.edu.et>
2025-12-03 07:51:23 +0300

    Refactor GameBoard initialization and add Player class

--- src/gebeta/domain/GameBoard.java
+++ src/gebeta/domain/GameBoard.java
@@ -2,26 +2,41 @@ package gebeta.domain;
 
 public class GameBoard {
 
+    public static final int TOTAL_PITS = 14;
+    public static final int INITIAL_STONE = 4;
+
+    // Player 1
+    public static final int P1_START = 0;
+    public static final int P1_END = 5;
+    public static final int P1_STORE = 6;
+
+    // Player 2
+    public static final int P2_START = 7;
+    public static final int P2_END = 12;
+    public static final int P2_STORE = 13;
+
     private Pit[] pits;
 
-    public static final int TOTAL_PITS = 12;  
-
-    public GameBoard(int initialStones) {
+    public GameBoard() {
         pits = new Pit[TOTAL_PITS];
 
-        // initialize each pit with stones
         for (int i = 0; i < TOTAL_PITS; i++) {
-            pits[i] = new Pit(i, initialStones);
+
+            // Stores start with 0 stones
+            if (i == P1_STORE || i == P2_STORE) {
+                pits[i] = new Pit(i, 0);
+            } else {
+                pits[i] = new Pit(i, INITIAL_STONE);
+            }
         }
     }
 
     public Pit getPit(int index) {
-        if (index < 0 || index >= TOTAL_PITS) {
-            System.out.println("Invalid pit index: " + index);
-        }
+        if (index < 0 || index >= TOTAL_PITS)
+            System.out.println("Invalid pit index " + index);
+
         return pits[index];
     }
-
     public int getStoneCount(int index) {
         return pits[index].getStoneCount();
     }
@@ -30,9 +45,15 @@ public class GameBoard {
         pits[index].setStoneCount(stoneCount);
     }
 
+    public int oppositePit(int index) {
+        // need to work on this for all pits execpt mancala
+        return 12 - index;
+    }
+
     public void printBoard() {
-        for (Pit pit : pits) {
-            System.out.println(pit);
+        for (Pit p : pits) {
+            System.out.println(p);
         }
     }
 }
+
---
+++ src/gebeta/domain/Player.java
@@ -0,0 +1,37 @@
+package gebeta.domain;
+
+public class Player {
+
+    private String name;
+    private int startPit;
+    private int endPit;
+    private int storePit;
+
+    public Player(String name, int startPit, int endPit, int storePit) {
+        this.name = name;
+        this.startPit = startPit;
+        this.endPit = endPit;
+        this.storePit = storePit;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getStartPit() {
+        return startPit;
+    }
+
+    public int getEndPit() {
+        return endPit;
+    }
+
+    public int getStorePit() {
+        return storePit;
+    }
+
+    // Score = the number of stones in the player's store pit
+    public int getScore(GameBoard board) {
+        return board.getPit(storePit).getStoneCount();
+    }
+}
