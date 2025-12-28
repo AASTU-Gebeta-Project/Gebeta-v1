9398bf6787adc65ad143c6ef4526a0dd86f47698
Emran Mohammed <emran.seid@aastustudent.edu.et>
2025-12-28 12:50:17 +0300

    Refactor GameManager to streamline game flow and improve AI integration

--- src/gebeta/controller/GameManager.java
+++ src/gebeta/controller/GameManager.java
@@ -23,47 +23,36 @@ public class GameManager{
         moveValidator = new MoveValidator();
         saveLoadSystem = new SaveLoadSystem();
     }
-    public void deleteSaveFile() {
-        java.io.File file = new java.io.File("gebeta_save.dat");
-        if (file.exists()) {
-            file.delete();
-            System.out.println("Save file deleted.");
-        }
-    }
-    public void endGamePermanently() {
-        this.board = null;
-        this.gameOver = true;
-        deleteSaveFile(); // This removes the .dat file so Resume disappears
-    }
-    public void startNewGame(String p1Name, String p2Name, boolean vsAI, Difficulty difficulty){
-        deleteSaveFile();
-        board = new GameBoard();
-        player1 = new Player(p1Name, GameBoard.P1_START, GameBoard.P1_END, GameBoard.P1_STORE, false);
 
-        player2 = new Player(p2Name, GameBoard.P2_START, GameBoard.P2_END,  GameBoard.P2_STORE, vsAI);
-        this.currentDifficulty = difficulty;
-        if (vsAI) {
-            this.ai = new GebetaAI(difficulty);
-        }
-        currentPlayer = player1;
-        gameOver = false;
+public void startNewGame(String p1Name, String p2Name, boolean vsAI, Difficulty difficulty){
+    deleteSaveFile();
+    board = new GameBoard();
+    player1 = new Player(p1Name, GameBoard.P1_START, GameBoard.P1_END, GameBoard.P1_STORE, false);
+    
+    player2 = new Player(p2Name, GameBoard.P2_START, GameBoard.P2_END,  GameBoard.P2_STORE, vsAI);
+    this.currentDifficulty = difficulty;
+    if (vsAI) {
+        this.ai = new GebetaAI(difficulty);
     }
-    public boolean playTurn(int pitIndex) {
-        if (gameOver) return false;
-
-        if (!moveValidator.isValidMove(board, currentPlayer, pitIndex)) return false;
-
+    currentPlayer = player1;
+    gameOver = false;
+}
+public boolean playTurn(int pitIndex) {
+    if (gameOver) return false;
+    
+    if (!moveValidator.isValidMove(board, currentPlayer, pitIndex)) return false;
+    
         Player opponent = (currentPlayer == player1) ? player2 : player1;
         boolean extraTurn = gameLogic.executeMove(board, currentPlayer, opponent, pitIndex);
-
-    
+        
+        
         if (gameLogic.isGameOver(board, player1, player2)) {
             gameLogic.collectRemainingStones(board, player1, player2);
             this.gameOver = true;
             deleteSaveFile();
             return true; // Return true to trigger UI refresh
         }
-
+        
         if (!extraTurn) {
             switchTurn();
         }
@@ -74,23 +63,23 @@ public class GameManager{
         currentPlayer = getOpponent(currentPlayer);
 
     }
-
+    
     public int getBestMoveForAI() {
         if (currentPlayer.isAI()) {
             return ai.findBestMove(board, currentPlayer, getOpponent(currentPlayer));
         }
         return -1;
     }
-   
+    
     
     public Player getWinner() {
         if (!gameOver) {
             return null;
         }
-
+        
         int score1 = player1.getScore(board);
         int score2 = player2.getScore(board);
-
+        
         if (score1 > score2) {
             return player1;
         } else if (score2 > score1) {
@@ -106,14 +95,14 @@ public class GameManager{
         }
         return player1.getScore(board) == player2.getScore(board);
     }
-//// save and load the game
+    //// save and load the game
     public void saveGame() {
         saveLoadSystem.saveGame(board, player1, player2, currentPlayer);
     }
-
+    
     public void loadGame() {
         SaveLoadSystem.LoadResult result = saveLoadSystem.loadGame();
-
+        
         if (result != null) {
             this.board = result.getBoard();
             this.player1 = result.getPlayer1();
@@ -121,11 +110,23 @@ public class GameManager{
             this.currentPlayer = result.getCurrentPlayer();
             this.gameOver = false;
             if (this.player2.isAI()) {
-                // You might need to save/load the difficulty too!
+                // by default load we load with difficulty medium
                 this.ai = new GebetaAI(this.currentDifficulty); 
             }
         }
         
+    }
+    public void deleteSaveFile() {
+        java.io.File file = new java.io.File("gebeta_save.dat");
+        if (file.exists()) {
+            file.delete();
+            System.out.println("Save file deleted.");
+        }
+    }
+    public void endGamePermanently() {
+        this.board = null;
+        this.gameOver = true;
+        deleteSaveFile(); 
     }
 
     public GameBoard getBoard() {
